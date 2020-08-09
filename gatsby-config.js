6630c4bfcaf550cd83aba53a96ec04e80d5f2e63
Serge Modin <modinsv@yandex.ru>
2020-08-09 19:07:05 +0300

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,12 @@ module.exports = {
     `gatsby-plugin-emotion`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-174947873-1",
+      },
+    },
   ],
 }
